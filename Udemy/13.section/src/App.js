cb1bc807354192c17584f0f4d69a4b51f0eca5d2
hm <squirrel309@naver.com>
2023-12-09 00:10:33 +0900

    23/12/09

--- Udemy/13.section/src/App.js
+++ Udemy/13.section/src/App.js
@@ -1,31 +1,39 @@
-import React  , {useState , useCallback, useEffect, useMemo }from 'react';
+import React  , {useState , useEffect, useMemo }from 'react';
 import Button from './components/UI/Button/Button';
+import { v4 as uuidv4 } from 'uuid';
 
 import './App.css';
 
 function App() {
   const [value , setValue ] = useState('');
-  const [list, setList ] = useState([]);
+  const [bulian, setBulian ] = useState(false);
+  const userObj = useMemo(()=>{
+    return [
+      {
+        user : bulian ? 'Hyunmin'  : 'hy'
+      }
+    ]
+  },[bulian])
+
+  useEffect(()=>{
+    console.log('App mount');
+  },[userObj]);
 
   const inputHandler = (e) =>{
       setValue(e.target.value);
   }
 
-  const result = () =>{
-      setList(prev => [...prev , { name: value}])
-      setValue('');
-  }
-
+  // const result = () =>{
+  //     setList(prev => [...prev , { user: value}])
+  //     setValue('');
+  // }
+  
   return (
     <div className="app">
-        {list.map((e)=>{
-            return <div>{e.name}</div>
-        })}
+        {userObj[0].user}
         <input type="text" value={value} onChange={inputHandler} />
-        {/* <button  onClick={result}>##</button> */}
-        <Button onClick={result}>hi</Button>
+        <Button onClick={() => setBulian(!bulian)}>hi</Button>
     </div>
-    
   );
 }
 
