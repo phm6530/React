459a37863b549dedaebccc723bb8175dd4ca9e20
hm <squirrel309@naver.com>
2023-12-16 00:58:55 +0900

    23/12/16 redux re setting

--- Udemy/19.redux/src/components/Counter.js
+++ Udemy/19.redux/src/components/Counter.js
@@ -1,39 +1,12 @@
 import classes from './Counter.module.css';
-import { useState } from 'react';
-import {  useSelector , useDispatch } from 'react-redux'
-import { INCREMENT , TOGGLE, DECREMENT } from '../store/index';
-
 
 const Counter = () => {
-  const [num ,setNum] = useState(0);
-  
-  const counter = useSelector(state => state.count);
-  const counterView = useSelector(state => state.showCounter);
-  
-  const dispatch =  useDispatch();
-
-  const toggleCounterHandler = () => {
-    dispatch({type: TOGGLE });
-  };
-  
-  const incrementHandlers = () =>{
-    dispatch({type: INCREMENT , amount : num})
-  }
-
-
-  const decrementHandler = () =>{
-    dispatch({type: DECREMENT})
-  }
+  const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      {counterView && <div className={classes.value}>{counter}</div>}
-      <div>
-          <input type="text" onChange={(e)=>setNum(+e.target.value)} />
-          <button style={{margin: '1rem'}} onClick={incrementHandlers}>Increment by 5</button>
-          <button onClick={decrementHandler}>Decrement</button>
-      </div>
+      <div className={classes.value}>-- COUNTER VALUE --</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- Udemy/19.redux/src/index.js
+++ Udemy/19.redux/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
-import store from './store/index'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// 공급자 최상 컴포넌트에서 지정 
-root.render(<Provider store={store}><App/></Provider>);
+root.render(<App />);
